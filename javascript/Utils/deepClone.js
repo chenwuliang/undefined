42ce065b5ad8395a798e41275534dcbecae988ab
chenwuliang <chenwuliang@360.cn>
2021-03-25 15:09:46 +0800

    a lot

--- javascript/Utils/deepClone.js
+++ javascript/Utils/deepClone.js
@@ -1,7 +1,8 @@
 
 function deepClone (obj) {
-    if (typeof obj !== 'object') return obj 
     if (typeof obj === null) return null
+    // if (typeof obj === 'function') return obj.bind()
+    if (typeof obj !== 'object') return obj 
     if (obj instanceof RegExp) return new RegExp(obj)
     if (obj instanceof Date) return new Date(obj)
     else {
@@ -21,7 +22,8 @@ let test = {
     date: new Date,
     reg: new RegExp,
     reg2: /^2$/,
-    err: new Error('err')
+    err: new Error('err'),
+    fn: () => {}
 }
 let obj2 = deepClone(test)
 // console.log(test)
--- javascript/this.js
+++ javascript/this.js
@@ -1,1 +1,34 @@
 // this指向问题
+
+// 情况一：纯粹的函数调用
+var x = 1;
+function test() {
+   console.log(this.x);
+}
+test();  // 1
+
+// 情况二：作为对象方法的调用
+// 函数还可以作为某个对象的方法调用，这时this就指这个上级对象。
+function test1() {
+    console.log(this.x);
+}
+var obj = {};
+obj.x = 1;
+obj.m = test1;
+obj.m(); // 1
+
+// 情况三 作为构造函数调用
+// 所谓构造函数，就是通过这个函数，可以生成一个新对象。这时，this就指这个新对象
+function test() {
+    this.x = 1;
+}
+var obj = new test();
+obj.x // 1
+
+// 箭头函数中 this
+var x = 1
+var test = () => {
+    const x = 2
+    console.log(this) // global
+    console.log(this.x)
+}
